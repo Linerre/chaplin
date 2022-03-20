0c74f035ab3d85ec906cbcd27cd9345b355d8865
Linerre <leon.errelin@outlook.com>
2022-03-21 00:57:20 +0800

    Add tailwindcss to the project
    Note: the `input.css` file will be compiled
    into `main.css` and used by index.html

---
+++ resources/public/css/tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: ["../*.html", "../js/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        green: {
+          750: '#0b7261'
+        },
+        purple: {
+          950: '#2e2459',
+          750: '#57068c'
+        },
+        red: {
+          650: '#a72145',
+        },
+        yellow: {
+          450: '#ffc832',
+        },
+        blue: {
+          350: '#4299bf',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
