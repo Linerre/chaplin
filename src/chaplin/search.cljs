b8603505948592ac0e75f7e946c6c081c32adb9f
Linerre <leon.errelin@outlook.com>
2022-04-04 01:11:21 +0800

    Combine components into one fn

--- src/chaplin/search.cljs
+++ src/chaplin/search.cljs
@@ -5,52 +5,57 @@
    [reagent.dom :as rdom]
    [reagent.core :as rc]))
 
+;; TODO:
+;; [X]. Combine the components into one form
+;; []. Send data to the server
+;; []. Running server to return a requested page
+
+(defn send-message
+  "Send user input as a map to the server."
+  [fields]
+  (GET "/result"
+       {:params @fields,
+        :handler #(.log js/console (str "response:" %)),
+        :err-handler #(.log js/console (str "error:" %))}))
+
 (defn search-box
   "Search box fregment"
   []
-  [:<>
-   [:label {:for "uquery"}]
-   [:input.w-half.border-solid.border-2.border-gray-600.p-2
-    {:type "text",
-     :id "uquery",
-     :name "uquery",
-     :placeholder " Title, Author, ISBN, barcode ... "}]])
-
-(defn drop-box
-  "Drop-down menu"
-  []
-  [:select#part-type.p-2.border-2.border-gray-900.bg-purple-950.text-white
-   {:id "ptype",
-    :name "ptype"}
-   [:option {:name "chapter", :value "chapter"} "Chapter"]
-   [:option {:name "section", :value "section"} "Section"]
-   [:option {:name "part",    :value "part"} "Part"]
-   [:option {:name "lesson",  :value "lesson"} "Lesson"]
-   [:option {:name "pages",   :value "pages"} "Pages"]])
+  (let [fields (rc/atom {})]
+    [:form.my-4.py-4
+     {:action "http://localhost:9322/result", :method "POST",
+      :class "w-full flex flex-row gap-x-4 justify-center my-4"}
+     [:select#part-type.p-2.border-2.border-gray-900.bg-purple-950.text-white
+      {:id "ptype",
+       :name "ptype"
+       :on-change #(swap! fields
+                          assoc :name (-> % .-target .-value))}
+      [:option {:name "chapter", :value "chapter"} "Chapter"]
+      [:option {:name "section", :value "section"} "Section"]
+      [:option {:name "part",    :value "part"} "Part"]
+      [:option {:name "lesson",  :value "lesson"} "Lesson"]
+      [:option {:name "pages",   :value "pages"} "Pages"]]
+     [:label {:for "uquery"}]
+     [:input.w-half.border-solid.border-2.border-gray-600.p-2
+      {:type "text",
+       :id "uquery",
+       :name "uquery",
+       :placeholder " Title, Author, ISBN, barcode ... "
+       :on-change #(swap! fields
+                          assoc :name (-> % .-target .-value))}]
+     [:button#btn.bg-yellow-450.text-black.font-bold.p-2
+      {:type "submit",
+       :on-click #(send-message fields),
+       :value "Search"}
+      "Search"]]))
 
 ;; in {:attr "val"}, "val" can be :val
 ;; but :val will be converted to "val" for DOM anyway
-(defn search-button
-  "Search button fragment"
-  []
-  [:<>
-   [:button#btn.bg-yellow-450.text-black.font-bold.p-2
-    {:type "submit"} "Search"]])
 
-(defn form-component
-  "Construct the form section"
-  []
-  [:form.my-4.py-4
-   {:action "/result", :method "GET",
-    :class "w-full flex flex-row gap-x-4 justify-center my-4"}
-   [drop-box]
-   [search-box]
-   [search-button]])
+
 
 (defn simple-component []
-  [:div
-   [form-component]
-   ])
+  [search-box])
 
 (defn render-simple []
   (rdom/render
