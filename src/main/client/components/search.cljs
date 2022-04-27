381a3b373d7d7291775066fbc693943183c9cd9a
Linerre <leon.errelin@outlook.com>
2022-04-27 13:28:15 +0800

    Redraw search box comp
    A re-agent/-act component does not have to a a very small
    fragement. It should be a reuseable widget, like nav-bar
    footer, buttons. Think of it as a HTML template for a
    section on the page.

--- src/main/client/components/search.cljs
+++ src/main/client/components/search.cljs
@@ -1,36 +1,47 @@
-(ns client.pages.index
+(ns client.components.search
   (:require
    [reagent.core :as rc]))
 
+(defn select-dropdwon
+  "Dropdwon menu for selecting parts."
+  []
+  [:select#part-type.p-2.border-2.border-gray-900.bg-purple-950.text-white
+      {:id "ptype",
+       :name "ptype"}
+      [:option {:name "chapter", :value "chapter"} "Chapter"]
+      [:option {:name "section", :value "section"} "Section"]
+      [:option {:name "part",    :value "part"} "Part"]
+      [:option {:name "lesson",  :value "lesson"} "Lesson"]
+      [:option {:name "pages",   :value "pages"} "Pages"]])
+
+(defn search-box
+  "Search box for user input, accepting title, ISBN, author and barcode."
+  []
+  [:label {:for "uquery"}]
+  [:input.w-half.border-solid.border-2.border-gray-600.p-2
+   {:type "text",
+    :id "uquery",
+    :name "uquery",
+    :placeholder " Title, Author, ISBN, barcode ... "}])
+
+(defn search-btn
+  "The search button for submitting user's query."
+  []
+  [:button#btn.bg-yellow-450.text-black.font-bold.p-2.hover:brightness-110
+      {:type "submit",
+       :on-click #(),
+       :value "Search"}
+   "Search"])
+
 ;; in {:attr "val"}, "val" can be :val
 ;; but :val will be converted to "val" for DOM anyway
-(defn search-box
-  "Search box fregment"
+(defn search-frame
+  "Search box fragment"
   []
   (let [fields (rc/atom {})]
     [:form.my-4.py-4
      {:action "http://localhost:9322/result", :method "POST",
       :class "w-full flex flex-row gap-x-4 justify-center my-"}
-     [:select#part-type.p-2.border-2.border-gray-900.bg-purple-950.text-white
-      {:id "ptype",
-       :name "ptype"
-       :on-change #(swap! fields
-                          assoc :name (-> % .-target .-value))}
-      [:option {:name "chapter", :value "chapter"} "Chapter"]
-      [:option {:name "section", :value "section"} "Section"]
-      [:option {:name "part",    :value "part"} "Part"]
-      [:option {:name "lesson",  :value "lesson"} "Lesson"]
-      [:option {:name "pages",   :value "pages"} "Pages"]]
-     [:label {:for "uquery"}]
-     [:input.w-half.border-solid.border-2.border-gray-600.p-2
-      {:type "text",
-       :id "uquery",
-       :name "uquery",
-       :placeholder " Title, Author, ISBN, barcode ... "
-       :on-change #(swap! fields
-                          assoc :name (-> % .-target .-value))}]
-     [:button#btn.bg-yellow-450.text-black.font-bold.p-2.hover:brightness-110
-      {:type "submit",
-       :on-click #(),
-       :value "Search"}
-      "Search"]]))
+     [select-dropdwon]
+     [search-box]
+     [search-btn]]))
