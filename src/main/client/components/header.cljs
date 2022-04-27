bb1e872029b18753e958d78150b3aea75096c595
Linerre <leon.errelin@outlook.com>
2022-04-27 13:16:54 +0800

    Turn the header into a comp

--- src/main/client/components/header.cljs
+++ src/main/client/components/header.cljs
@@ -3,17 +3,17 @@
   (:require
    [reagent.core :as r]))
 
-(defn site-header
+(defn header-frame
   "The header area showing the site's name."
   []
-  [:<>
+  [:header
    [:div.w-full.my-4.p-2.flex.items-center.justify-center
     [:span.text-5xl.z-10 "Chap"]
     [:img.h-22.-ml-6.-mr-8.z-0
      {:alt "stick icon",
       :src "img/stick-icon.svg"}]
     [:span.text-5xl.z-10 "in"]]
-   [:div.w-full.py-2.my-2.flex.justify-center.gap-x-2
+   [:div.w-full.py-2.my-4.flex.justify-center.gap-x-2
     [:i.fi-page.ml-2]
     [:span.pb-1.border-b-4.border-red-650
      "Find scanned PDF chapters, sections, or pages"]]])
