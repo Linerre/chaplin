2e4670227f1b938974670bc879495df015e646eb
Linerre <leon.errelin@outlook.com>
2022-04-30 00:07:03 +0800

    Introduce re-frame

--- src/main/client/app.cljs
+++ src/main/client/app.cljs
@@ -4,7 +4,10 @@
    [clojure.string :as str]
    [goog.dom :as gdom]
    [reagent.dom :as rdom]
-   [client.router :as router]))
+   [re-frame.core :as rf]
+   [client.pages.index]
+   [client.router :as router]
+   [client.events.search]))
 
 (defn mount [& args]
   (rdom/render router/router-component
@@ -12,10 +15,13 @@
 
 (defn -main [& args]
   (router/init-router)
+  (rf/dispatch [:search/init])
   (mount)
   (.log js/console "App started!"))
 
 (defn after-load [& args]
   (router/init-router)
+  ;; (rf/clear-subscription-cache!)
+  (rf/dispatch-sync [:search/init])
   (mount)
   (.log js/console "Page refreshed!"))
