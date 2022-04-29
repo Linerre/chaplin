ac8e18500ff9ab113edad1c67277aaf0868d56ed
Linerre <leon.errelin@outlook.com>
2022-04-29 21:48:51 +0800

    Set up client-side router
    There are two links in the top nav that should be included in
    the client side routes: Home (the icon) and About.
    
    The key to client side router is the history API. reitit offers
    a module (frontend.easy) to that end

--- src/main/client/components/nav.cljs
+++ src/main/client/components/nav.cljs
@@ -1,43 +1,35 @@
 (ns client.components.nav
+  "The top navigation bar. It holds two special routes:
+  1. Home page (the Chaplin icon)
+  2. About page"
   (:require
-   [reagent.core]))
+   [reitit.frontend.easy :as rfe]))
 
-(def nav-urls "Links for navigation bar."
-  {:copyright {:url "https://guides.nyu.edu/copyright",
-               :txt "Copyright"},
-   :policy    {:url "https://shanghai.nyu.edu/academics/library/policy",
-               :txt "Policy"},
-   :scanning  {:url "https://library.nyu.edu/nyu-returns/collections-access/#scanning",
-               :txt "Scanning"},
-   :reserve   {:url "https://shanghai.nyu.edu/academics/library/services/course-reserves",
-               :txt "Linking Service"},
-   :contact   {:url "mailto:zl37@nyu.edu",
-               :txt "Contact"},
-   :about     {:url "https://github.com/Linerre/chaplin",
-               :txt "About"}})
+;; This is an example demenstrating that a component doesn't
+;; have to follow the pattern of "children elements + frame".
+;; Instead, a component should be a reusable block, even the
+;; component fn is a bit lengthy.
 
-(defn main-icon
-  "The site icon, clickable and linking to the index page."
-  []
-  [:div.flex.my-2.px-2.text-left
-   [:a {:href "http://localhost:9321"}
-    [:img.w-12 {:alt "Site Icon - Chaplin"
-                :src "img/chaplin-200x200.png"}]]])
-
-(defn nav-items
-  "Top navigation bar."
-  [items-map]
-  [:ul#nav-items.flex.gap-x-6.justify-center
-   (for [item (seq (keys items-map))]
-     ^{:key item} [:li.mx-1
-                   [:a.underline.hover:text-blue-350
-                    {:href (get-in items-map [item :url]),
-                     :target "_blank"}
-                    (get-in items-map [item :txt])]])])
-
-(defn nav-bar-frame
-  "Frame holds the top navigation bar."
-  []
-  [:div.my-2.gap-2.flex.flex-col.sm:flex-row.flex-wrap.items-center.justify-center
-   [main-icon]
-   [nav-items nav-urls]])
+(defn top-nav
+  "The top navigation bar. It should appear on both index and about page."
+  [home about]
+  [:nav#app-nav.my-2.gap-4.flex.flex-col.sm:flex-row.flex-wrap.items-center.justify-center
+   [:div#nav-icon.flex.my-2.px-2
+    [:a {:href (rfe/href home)}
+     [:img.w-12 {:alt "Site Icon - Chaplin",
+                 :src "/img/chaplin-200x200.png"}]]]
+   [:ul#nav-items.flex.gap-x-10.justify-center
+    [:li [:a {:href "https://guides.nyu.edu/copyright",
+              :target "_blank",
+              :class "underline hover:text-blue-350"} "Copyright"]]
+    [:li [:a {:href "https://shanghai.nyu.edu/academics/library/policy",
+              :target "_blank",
+              :class "underline hover:text-blue-350"} "Policy"]]
+    [:li [:a {:href "https://library.nyu.edu/nyu-returns/collections-access/#scanning",
+              :target "_blank",
+              :class "underline hover:text-blue-350"} "Scanning"]]
+    [:li [:a {:href "https://shanghai.nyu.edu/academics/library/services/course-reserves",
+              :target "_blank",
+              :class "underline hover:text-blue-350"} "Linking Services"]]
+    [:li [:a {:href (rfe/href about)
+              :class "underline hover:text-blue-350"} "About"]]]])
--- src/main/client/router.cljs
+++ src/main/client/router.cljs
@@ -1,9 +1,56 @@
 (ns client.router
   "Client-side router"
   (:require
-   [chaplin.client.pages.index :refer [search-box]]
-   [reitit.frontend :as rf]))
+   [client.components.nav :as nav]
+   [client.components.footer :as footer]
+   [client.pages.about :as about]
+   [client.pages.index :as index]
+   [reagent.core :as r]
+   [reitit.frontend :as rf]
+   [reitit.frontend.easy :as rfe]))
 
-(def fd-routes  ;; frontend routes
-  [["/" {:name ::index, ;; ::index => :curr-ns/index == chaplin.router/index
-         :view search-box}]])
+;; the state of router
+(def match (r/atom nil))
+
+;; on-navigate fn as 2nd arg for rfe/start!
+(defn on-navigate [new-match]
+  (when new-match (reset! match new-match)))
+
+(defn current-page
+  "Index page."
+  [index about]
+  [:div#app.h-fit.w-screen.mt-4
+   [nav/top-nav index about]
+   (when @match
+     (let [view (get-in @match [:data :view])
+           page (get-in @match [:data :text])]
+      ;; @match changes then view reloads
+       [view @match]))])
+
+;; frontend routes
+(def routes
+  ;; ::index => :curr-ns/index
+  ;; == client.router/index
+  [["/" {:name ::index,
+         :view index/index-page,
+         :text "Home"}]
+
+   ["/about" {:name ::about,
+              :view about/about-page,
+              :text "About"}]])
+
+;; router fn optionally accepts a 3rd arg
+;; which is a map
+(def router
+  (rf/router routes {:data {}}))
+
+;; to run before the app-mounting
+(defn init-router []
+  (rfe/start!
+   router
+   on-navigate
+   {:use-fragment true}))
+
+;; component to pass to rdom/render in app.cljs
+(defn router-component []
+  [current-page ::index ::about])
