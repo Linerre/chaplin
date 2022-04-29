8aa109531959068842eb5b344258b06901202ffe
Linerre <leon.errelin@outlook.com>
2022-04-29 21:51:59 +0800

    Clean the app entry point
    The router returns a render fn that constructs the current page,
    based on path/url. This fn in turn is passed to reagent.core
    and gets rendered and mounted/inserted to the mounting point.

--- src/main/client/app.cljs
+++ src/main/client/app.cljs
@@ -1,36 +1,21 @@
 (ns client.app
   (:require
    [ajax.core :refer [POST GET]]
+   [clojure.string :as str]
    [goog.dom :as gdom]
    [reagent.dom :as rdom]
-   [client.components.nav :as nav]
-   [client.components.header :as hd]
-   [client.pages.index :as index]))
+   [client.router :as router]))
 
-(defn render-nav []
-  (rdom/render
-   [nav/nav-bar-frame]
-   (.getElementById js/document "top-nav")))
+(defn mount [& args]
+  (rdom/render router/router-component
+               (.getElementById js/document "app-mnt-pnt")))
 
-(defn render-header []
-  (rdom/render
-   [hd/site-header]
-   (.getElementById js/document "header")))
+(defn -main [& args]
+  (router/init-router)
+  (mount)
+  (.log js/console "App started!"))
 
-(defn render-search []
-  (rdom/render
-    [index/search-box]
-    ;; (js/document.getElementById "app")
-    (. js/document (getElementById "app"))))
-
-(defn -main []
-  (.log js/console "App started!")
-  (render-nav)
-  (render-header)
-  (render-search))
-
-(defn after-load! []
-  (.log js/console "Page refreshed!")
-  (render-nav)
-  (render-header)
-  (render-search))
+(defn after-load [& args]
+  (router/init-router)
+  (mount)
+  (.log js/console "Page refreshed!"))
