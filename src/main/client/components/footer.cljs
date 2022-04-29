15e022f194bd9b6118e5b58d8d278abb3bb53fdb
Linerre <leon.errelin@outlook.com>
2022-04-30 00:05:53 +0800

    Fix "div should not be embedded in p"

--- src/main/client/components/footer.cljs
+++ src/main/client/components/footer.cljs
@@ -8,8 +8,8 @@
    {:class (if (= page "Home")
              "fixed bottom-0 left-0"
              "mt-2")}
-   [:p.w-triq.mx-auto.text-center.italic.text-slate-100
-    [:span.text-sm.text-white
+   [:p.w-triq.mx-auto.italic.text-sm.text-slate-50.text-center.font-bold
+    [:span
      quo]]
    [:div.h-fit.pt-4.pb-2.mt-4.flex.flex-col.gap-y-1.text-sm
     [:p.text-center.text-white "Built with "
@@ -17,9 +17,11 @@
    {:href "https://clojure.org/index",
     :target "_blank"}
    "Clojure(Script)"]]
-    [:p.flex.gap-x-2.justify-center.relative.text-center.text-white
-     [:span "Special thanks to Jade for her hard work"]
-     [:span.absolute.left-8 "© 2022 Leon Lin"]
+    [:div.flex.gap-x-2.justify-center.relative.text-center.text-white
+     [:div
+      [:span "Special thanks to Jade for her hard work"]]
+     [:div.absolute.left-8
+      [:span "© 2022 Leon Lin"]]
      [:div.absolute.right-8.flex.gap-x-4.justify-center.text-lg
       [:a {:href "https://shanghai.nyu.edu/academics/library",
            :target "_blank"}
@@ -28,5 +30,4 @@
        [:i.fi-mail]]
       [:a {:href "https://github.com/Linerre/chaplin",
            :target "_blank"}
-       [:i.fi-social-github]]
-      ]]]])
+       [:i.fi-social-github]]]]]])
