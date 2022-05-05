f79291d3e7db1b70d5e14fd0bfc8b75717fded28
Linerre <leon.errelin@outlook.com>
2022-05-05 18:15:34 +0800

    Update about page
    1. Add metadata to some string vars
    2. Add acknowledgements section

--- src/main/client/pages/about.cljs
+++ src/main/client/pages/about.cljs
@@ -7,29 +7,36 @@
    [client.components.header :as header]
    [client.components.footer :as footer]))
 
-(def background
-  "Since the outbreak of COVID-19, NYU Shanghai library has seen an increasing demand for the scan of book chapters or  pages.")
+(def ^:private background
+  "Since the outbreak of COVID-19, NYU Shanghai library has seen an increasing demand for scan of book chapters or  pages.")
 
-(def reasons
-  "This is partly because many courses have moved online, and partly because the shipping channels have been significantly disrupted. In such cases, scanning a small part of a book for courses has proven to be a faster and more flexible solution.")
+(def ^:private reasons
+  "This is partly because many courses have moved online and partly because the shipping channels have been significantly disrupted. In such cases, scanning a small part of a book for courses has proven to be a faster and more flexible solution.")
 
-(def problem
+(def ^:private problem
   "However, as the number of the scanned files grows over time, it also becomes harder for the library staff to track:")
 
-(def difficulties
+(def ^:private challenges
   [
    "Which chapter (or part) from which book has been scanned?",
    "Where is a specific scan stored in Google Drive?"
    ])
 
-(def aim
+(def ^:private aim
   "This project aims to solve the above problem.")
+
+(def ^:private thanks-jade
+  "I would like to express my appreciation to Jade, the student worker at NYU Shanghai Library, for her invaluable contribution to the database of this application. This project would have been impossible without her effort and dedication.")
+
+(def ^:private thanks-tianshu
+  "I would also like to thank Tianshu. His advice on the selection of tech stack for this project has been a great souce of help and inspiration.")
 
 (def lorem
   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
 
-(def quo-long
+(def ^:private quo-long
   "I feel I am privileged to express a hope. The hope is this: that we shall have peace throughout the world, that we shall abolish wars and settle all international differences at the conference table, that we shall abolish all atom and hydrogen bombs before they abolish us. The future of the modern world demands modern thinking. Therefore, let us use the full force of our intelligence instead of obsolete homicidal methods in settling our international differences.")
+
 
 (defn about-para
   "Paragraphs on the about page."
@@ -41,14 +48,18 @@
   "The frame holding content for the about page."
   []
   [:div#app-about.w-screen
-   [:div.w-meet.flex.flex-col.justify-center.py-2.px-8.mx-auto
+   [:div.w-meet.flex.flex-col.justify-center.py-2.px-8.mx-auto.mb-6
     [header/about-header "Motivation"]
     [about-para background]
     [about-para reasons]
     [about-para problem]
     [:ul.list-disc.w-near.pl-16.mx-auto.my-1
-     (for [diff difficulties]
-       ^{:key diff}
-       [:li.normal diff])]
+     (for [cha challenges]
+       ^{:key cha}
+       [:li.normal cha])]
     [about-para aim]]
+   [:div.w-meet.flex.flex-col.justify-center.py-2.px-8.mx-auto.mb-6
+    [header/about-header "Acknowledgements"]
+    [about-para thanks-jade]
+    [about-para thanks-tianshu]]
    [footer/footer-frame "About" quo-long]])
