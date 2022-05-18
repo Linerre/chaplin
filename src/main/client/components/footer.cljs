0d8cf4ba880d1435fc494035581c51660ba936ac
Linerre <leon.errelin@outlook.com>
2022-05-18 21:00:52 +0800

    Re-order the footer element on small screens

--- src/main/client/components/footer.cljs
+++ src/main/client/components/footer.cljs
@@ -10,9 +10,12 @@
    ;;  [:p.w-full.mx-auto.text-center.text-sm
    ;;   [:span.italic.text-slate-50 quo]]]
    [:div.flex.flex-col.justify-center.items-center.pb-1.tb:items-end.tb:flex-row
-    [:p.flex-1.text-left.ml-4.text-white.text-xs.sm:text-sm
+    ;; copyright
+    [:p.order-2.sm:order-1.flex-1.text-center.sm:text-left.sm:ml-4.text-white.text-xs.sm:text-sm
      [:span "Copyright © 2022 Leon Lin"]]
-    [:div.flex-1.flex.flex-col.justify-center
+
+    ;; thanks
+    [:div.order-1.sm:order-2.flex-1.flex.flex-col.justify-center
      [:p.mx-auto.text-center.text-white.text-xs.sm:text-sm
       [:span "Special thanks to Jade"]]
      [:p.text-center.text-white.text-xs.sm:text-sm
@@ -21,12 +24,14 @@
        {:href "https://clojure.org/index",
         :target "_blank"}
        "Clojure(Script)"]]]
-    [:p.flex-1.flex.gap-x-4.justify-end.mr-4.text-white.text-lg
-      [:a {:href "https://shanghai.nyu.edu/academics/library",
-           :target "_blank"}
-       [:i.fi-home]]
-      [:a {:href "mailto:zl37@nyu.edu"}
-       [:i.fi-mail]]
-      [:a {:href "https://github.com/Linerre/chaplin",
-           :target "_blank"}
-       [:i.fi-social-github]]]]])
+
+    ;; icons
+    [:p.order-3.flex-1.flex.gap-x-4.justify-end.mr-4.text-white.text-lg
+     [:a {:href "https://shanghai.nyu.edu/academics/library",
+          :target "_blank"}
+      [:i.fi-home]]
+     [:a {:href "mailto:zl37@nyu.edu"}
+      [:i.fi-mail]]
+     [:a {:href "https://github.com/Linerre/chaplin",
+          :target "_blank"}
+      [:i.fi-social-github]]]]])
