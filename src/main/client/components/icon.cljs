1ec7cd9355fa7ca04293ada753e618c30fdf5594
Linerre <leon.errelin@outlook.com>
2022-05-21 20:12:29 +0800

    Finish basic layout for the result page
    When data gets fetched from server db, it will be displayed on this page

--- src/main/client/components/icon.cljs
+++ src/main/client/components/icon.cljs
@@ -1,19 +1,13 @@
 (ns client.components.icon
   (:require
-   [reagent.core :as rc]
-   [reagent.dom :as rdom]))
+   [reitit.frontend.easy :as rfe]))
 
 (defn chaplin-icon []
-  [:div.flex.my-2.px-2
-   [:a {:href "http://localhost:9321/"}
-    [:img.w-12
+  [:div.flex-initial.flex.my-2.px-2
+   [:a.w-full.h-auto {:href (rfe/href :home)}
+    [:img.w-12.h-auto
      {:alt "Site Icon - Chaplin",
       :src "img/chaplin-200x200.png"}]]])
 
 (defn magnifying-glass []
   [:i.fi-magnifying-glass.ml-2])
-
-(defn render-test []
-  (rdom/render
-   [chaplin-icon]
-   (. js/document (getElementById "test"))))
--- src/main/client/pages/result.cljs
+++ src/main/client/pages/result.cljs
@@ -1,12 +1,45 @@
-(ns chaplin.client.pages.result
+(ns client.pages.result
   (:require
-   [chaplin.components.icon :as icon]))
+   [client.components.icon :as icon]
+   [client.components.search :refer [search-frame]]))
 
-;; TODO:
-;; [] icon at upper left
-;; [] search bar top
-;; [] body part
+(defn result-page [match]
+  ;; q(uery) comes from route parameters
+  (let [{:keys [query]} (:parameters match) ]
+    [:div.w-screen.h-screen
+     ;; top bar: icon (back to home) + search bar
+     [:div.w-full.flex.border-b-2
+      ;; icon
+      [icon/chaplin-icon]
+      ;; search bar
+      [:div.flex-1.flex.flex-col.justify-center.mx-auto
+       [search-frame]
+       ;; checkbox
+       [:div.flex.justify-center.items-center.gap-x-4.p-2.mx-auto
+        [:span.checkbox-wrapper
+         [:input.mr-1 {:type "checkbox", :id "wholeword",
+                  :name "wholeword", :value "Wholeword"}]
+         [:label {:for "wholeword"} "Wholeword"]]
+        [:span.checkbox-wrapper
+         [:input.mr-1 {:type "checkbox", :id "wholeword",
+                  :name "wholeword", :value "Wholeword"}]
+         [:label {:for "wholeword"} "Wholeword"]]
 
-(defn result-page []
-  [:div
-   [icon/chaplin-icon]])
+        ;;
+        ]]
+
+      ]
+
+     ;; body = sidebar + result list
+     [:div.w-full.flex
+      ;; sidebar
+      [:div.flex-initial.grow-0.w-quar.max-w-thir.p-2.border-r-2
+       [:p "Public data suggests that several anonymous crypto investors profited from inside knowledge of when tokens would be listed on exchanges."]]
+
+      ;; result list
+      [:ul.px-2
+       (for [x (take 10 (range 50))]
+         ^{:key x}
+         [:li x])]
+      ]])
+  )
