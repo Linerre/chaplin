777d6cf7b7c8c601f295f4a289b586fd29615b3e
Linerre <leon.errelin@outlook.com>
2022-05-26 23:44:50 +0800

    Prepare for filtering function
    TODO: consider moving db to a separate file

--- src/main/client/components/result.cljs
+++ src/main/client/components/result.cljs
@@ -1,12 +1,32 @@
 (ns client.components.result
   "The area displying a single result."
   (:require
+   [clojure.string :as cstr]
    [re-frame.core :refer [dispatch subscribe]]))
+
+(defn result-filter
+  "Checkbox for filtering search results."
+  [part-type]
+  [:div.flex.items-center.gap-x-2
+   [:input.w-4.h-4.cursor-pointer
+    {:type "checkbox",
+     :id part-type,
+     :name part-type,
+     :value part-type,
+     :on-change (fn []
+                  (dispatch [:result/checked-type part-type])
+                  (dispatch [:result/enabled-filter]))
+     :checked @(subscribe [:result/checked? part-type])}]
+   [:label.capitalize {:for part-type} (cstr/replace (str part-type) #":" "")]]
+  )
 
 (defn result-item
   "Result item component, based on part-type"
   [part-type]
   [:div.w-near.h-fit.my-2.mx-2.p-1.flex.items-center.gap-x-2.border-2.border-sky-500
+   {:class (fn [] (if-let [display? @(subscribe [:search/checked?])]
+                    ""
+                    "hidden"))}
    ;; cover
    [:div.shrink-0.grow-0.flex
     [:img.w-36.h-48
@@ -15,8 +35,7 @@
     ]
    ;; Citation
    [:div.flex-auto.shrink-1.mx-1.p-1
-    ;; [:p "Database System Concepts"]     ; title
-    [:p
+    [:p                                 ; title
      [:span.flex.gap-x-4.items-start
       [:i.fi-book.text-lg]
       [:span "400 million customers : the experiences--some happy, some sad of an American in China and what they taught him"]]]
