a706a8373cb9c339ae4f5a49286bca92de0effe2
Linerre <leon.errelin@outlook.com>
2022-05-29 16:55:01 +0800

    Add footer to the result page
    This page is a special case where the body height changes.
    To keep footer always at the bottom means it must "attach"
    to the body but relatively stay at the bottom of the frame.
    
    See this article for the trick in detail:
    www.freecodecamp.org/news/how-to-keep-your-footer-where-it-belongs-59c6aa05c59c

--- src/main/client/pages/result.cljs
+++ src/main/client/pages/result.cljs
@@ -3,15 +3,14 @@
    [client.components.icon :as icon]
    [client.components.search :refer [search-frame]]
    [client.components.result :as result]
+   [client.components.footer :refer [footer-frame]]
    [re-frame.core :as re-frame]))
 
 (defn result-page []
-  ;; q(uery) comes from route parameters
-
   (let [u-query (re-frame/subscribe [:search/user-query])]
-    [:div.w-screen.h-screen
+    [:div.w-screen.min-h-screen.bg-slate-25.relative
      ;; top bar: icon (back to home) + search bar
-     [:div.w-full.border-b-2.flex.px-2.mt-2.sm:px-16
+     [:div.w-full.border-b-2.flex.px-2.mt-2.sm:px-16.bg-white
       ;; icon
       [icon/chaplin-icon]
       ;; search bar
@@ -21,12 +20,10 @@
        [:div.w-meet.flex.justify-around.items-center.gap-x-3.mx-auto.pb-3
         ;; from most common to lest
         (for [pt [:chapter :section :pages :lesson :part]]
-          ^{:key pt} [result/result-filter pt])
-]]
-      ]
+          ^{:key pt} [result/result-filter pt])]]]
 
      ;; body = sidebar + result list
-     [:div.w-full.bg-slate-25           ; body outer frame
+     [:div.w-full.pt-2.pb-20            ; body outer frame, pb-v = footer height
       [:div.w-near.mx-auto.flex         ; body inner frame
        ;; sidebar
        [:div.flex-initial.grow-0.w-quar.max-w-thir.p-2.border-r-2
@@ -44,5 +41,8 @@
        ]
 
 
-      ]])
+      ]
+
+     ;; footer
+     [footer-frame "result"]])
   )
